2a5d982f1971b66142369ae6b777fdfae5d653b3
pramodsharma92 <pramod.a.sharma46@gmail.com>
2017-11-17 09:18:08 +0530

    Rename ad.ps1 to ADpowershellscrpit.ps1

---
+++ ADpowershellscrpit.ps1
@@ -0,0 +1,3 @@
+﻿Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDEmployment
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Default" -DomainName "pramodexample.com" -InstallDns: $true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion: $false -SysvolPath "C:\Windows\SYSVOL" -Force: $true -SafeModeAdministratorPassword (ConvertTo-SecureString 'P@sswOrd' -AsPlainText -Force)
